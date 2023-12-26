4879d73174ad39a79ff0253341d859bfc395b532
LcuasLedesma <lucasfidel99@gmail.com>
2023-12-26 11:47:40 -0300

    player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -73,12 +73,113 @@ export const CurrentSong = ({ image, title, artists }) => {
 		</div>
 	)
 }
+
+export const SongControl = ({ audio }) => {
+	const [currentTime, setCurrentTime] = useState(0)
+
+	useEffect(() => {
+		audio.current.addEventListener(
+			'timeupdate',
+			handleTimeUpdate
+		)
+		return () => {
+			audio.current.removeEventListener(
+				'timeupdate',
+				handleTimeUpdate
+			)
+		}
+	}, [])
+
+	const handleTimeUpdate = () => {
+		setCurrentTime(audio.current.currentTime)
+	}
+
+	const formatTime = (time) => {
+		if (time == null) return `0:00`
+
+		const seconds = Math.floor(time % 60)
+		const minutes = Math.floor(time / 60)
+
+		return `${minutes}:${seconds
+			.toString()
+			.padStart(2, '0')}`
+	}
+
+	const duration = audio?.current?.duration ?? 0
+
+	return (
+		<div className='flex gap-x-3 text-xs'>
+			<span className='opacity-50'>
+				{formatTime(currentTime)}
+			</span>
+
+			<Slider
+				defaultValue={[0]}
+				max={audio?.current?.duration ?? 0}
+				min={0}
+				value={[currentTime]}
+				className='w-[300px]'
+				onValueChange={(value) => {
+					const [newCurrentTime] = value
+					audio.current.currentTime = newCurrentTime
+				}}
+			/>
+
+			<span className='opacity-50'>
+				{formatTime(duration)}
+			</span>
+		</div>
+	)
+}
+
+const VolumeControl = () => {
+	const volume = usePlayerStore((state) => state.volume)
+	const setVolume = usePlayerStore(
+		(state) => state.setVolume
+	)
+	const previouesVolumeRef = useRef(volume)
+
+	const isVolumeSilenced = volume < 0.1
+
+	const handleClickVolumen = () => {
+		if (isVolumeSilenced) {
+			setVolume(previouesVolumeRef.current)
+		} else {
+			previouesVolumeRef.current = volume
+			setVolume(0)
+		}
+	}
+
+	return (
+		<div className='flex justify-center gap-x-2'>
+			<button
+				className='opacity-70 hover:opacity-100 transition'
+				onClick={handleClickVolumen}>
+				{isVolumeSilenced ? <VolumeSilence /> : <Volume />}
+			</button>
+			<Slider
+				defaultValue={[100]}
+				max={100}
+				min={0}
+				value={[volume * 100]}
+				className='w-[95px]'
+				onValueChange={(value) => {
+					const [newVolume] = value
+					const volumeValue = newVolume / 100
+					setVolume(volumeValue)
+				}}
+			/>
+		</div>
+	)
+}
+
 export function Player() {
 	const {
 		isPlaying,
 		setIsPlaying,
 		currentMusic,
 		setIsCurrentMusic,
+		volume,
 	} = usePlayerStore((state) => state)
 	const audioRef = useRef()
 	const volumeRef = useRef(1)
@@ -94,10 +195,14 @@ export function Player() {
 		if (song) {
 			const src = `/music/${playlist?.id}/0${song.id}.mp3`
 			audioRef.current.src = src
-			audioRef.current.volume = volumeRef.current
+			audioRef.current.volume = volume
 			audioRef.current.play()
 		}
 	}, [currentMusic])
+
+	useEffect(() => {
+		audioRef.current.volume = volume
+	}, [volume])
 
 	const handleClick = () => {
 		setIsPlaying(!isPlaying)
@@ -109,31 +214,20 @@ export function Player() {
 			</div>
 
 			<div className='grid place-content-center gap-4 flex-1'>
-				<div className='flex justify-center'>
+				<div className='flex justify-center flex-col items-center'>
 					<button
 						onClick={handleClick}
 						className='bg-white rounded-full p-2'>
 						{isPlaying ? <Pause /> : <Play />}
 					</button>
+					<SongControl audio={audioRef} />
+					<audio ref={audioRef} />
 				</div>
 			</div>
 
 			<div className='grid place-content-center'>
-				<Slider
-					defaultValue={[100]}
-					max={100}
-					min={0}
-					className='w-[95px] '
-					onValueChange={(value) => {
-						const [newVolume] = value
-						const volumeValue = newVolume / 100
-						volumeRef.current = volumeValue
-						audioRef.current.volume = volumeValue
-					}}
-				/>
+				<VolumeControl />
 			</div>
-
-			<audio ref={audioRef} />
 		</div>
 	)
 }
--- src/store/playerStore.js
+++ src/store/playerStore.js
@@ -1,6 +1,8 @@
 import { create } from 'zustand'
 
 export const usePlayerStore = create((set) => ({
+	volume: 1,
+	setVolume: (volume) => set({ volume }),
 	isPlaying: false,
 	currentMusic: { playlist: null, song: null, songs: [] },
 	setIsPlaying: (isPlaying) => set({ isPlaying }),
